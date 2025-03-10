493dc96b7328cda0f0865bd2770a149914e0a670
Benjamin Zores <benjamin.zores@gmail.com>
2025-03-10 18:36:19 +0100

    Revert "updated apt repositories management logic, deprecate dalet_baseos_packages_mirror_location public variable, add dalet_baseos_packages_mirror_dalet_repository one and ensure proper local proxy compatibility"
    This reverts commit f6f4e6e123b2445f6c928558281ae59d3ce26ea1.

--- pre_commit_hooks/api_checker.py
+++ pre_commit_hooks/api_checker.py
@@ -7,29 +7,29 @@ import re
 
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument('filenames', nargs='*', help='Filenames to check.')
+    parser.add_argument('-i', '--input', action='store', default=None, help='YAML-formatted input variable file')
+    parser.add_argument('-o', '--output', action='store', default="API.md", help='Markdown-formatted API documentation file')
     args = parser.parse_args(argv)
 
     if args.input == None:
         return 1
 
     retval = 0
-    for filename in args.filenames:
-        try:
-            with open(filename) as varfile:
-                vars = yaml.safe_load(varfile.read())
+    try:
+        with open(args.input) as varfile:
+            vars = yaml.safe_load(varfile.read())
 
-                with open('API.md') as docfile:
-                    doc = docfile.read()
-                    for key in vars.keys():
-                        pattern = f'<!-- block {key} -->'
-                        match = re.search(pattern, doc)
-                        if not bool(match):
-                            print(f'Found un-documented variable {key}, please update {args.output} file.')
-                            retval += 1
-        except SyntaxError as e:
-            print(e)
-            retval = 1
+            with open(args.output) as docfile:
+                doc = docfile.read()
+                for key in vars.keys():
+                    pattern = f'<!-- block {key} -->'
+                    match = re.search(pattern, doc)
+                    if not bool(match):
+                        print(f'Found un-documented variable {key}, please update {args.output} file.')
+                        retval += 1
+    except SyntaxError as e:
+        print(e)
+        retval = 1
 
     return retval
 
