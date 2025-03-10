839d8f524921d5e2cd60dd5e15d56209734be56d
Benjamin Zores <benjamin.zores@gmail.com>
2025-03-10 17:56:52 +0100

    correct hook file

--- pre_commit_hooks/api_checker.py
+++ pre_commit_hooks/api_checker.py
@@ -1,33 +1,37 @@
-#!/usr/bin/env python3
+from __future__ import annotations
 
+import argparse
 import sys
 import yaml
 import re
-import argparse
 
-# main
-if __name__ == "__main__":
-    # parse command-line
-    ps = argparse.ArgumentParser()
-    ps.add_argument('-i', '--input', action='store', default=None, help='YAML-formatted input variable file')
-    ps.add_argument('-o', '--output', action='store', default="API.md", help='Markdown-formatted API documentation file')
-    args = ps.parse_args()
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', action='store', default=None, help='YAML-formatted input variable file')
+    parser.add_argument('-o', '--output', action='store', default="API.md", help='Markdown-formatted API documentation file')
+    args = parser.parse_args(argv)
 
-    # check for args
     if args.input == None:
-        sys.exit(1)
+        return 1
 
-    errors_count = 0
-    with open(args.input) as varfile:
-        vars = yaml.safe_load(varfile.read())
+    retval = 0
+    try:
+        with open(args.input) as varfile:
+            vars = yaml.safe_load(varfile.read())
 
-        with open(args.output) as docfile:
-            doc = docfile.read()
-            for key in vars.keys():
-                pattern = f'<!-- block {key} -->'
-                match = re.search(pattern, doc)
-                if not bool(match):
-                    print(f'Found un-documented variable {key}, please update {args.output} file.')
-                    errors_count += 1
+            with open(args.output) as docfile:
+                doc = docfile.read()
+                for key in vars.keys():
+                    pattern = f'<!-- block {key} -->'
+                    match = re.search(pattern, doc)
+                    if not bool(match):
+                        print(f'Found un-documented variable {key}, please update {args.output} file.')
+                        retval += 1
+    except SyntaxError as e:
+        print(e)
+        retval = 1
 
-    sys.exit(errors_count)
+    return retval
+
+if __name__ == '__main__':
+    raise SystemExit(main())
