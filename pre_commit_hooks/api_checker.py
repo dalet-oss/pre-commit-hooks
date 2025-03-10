f785b0c50ae1803a345df179056dac88afae3d57
Benjamin Zores <benjamin.zores@gmail.com>
2025-03-10 18:36:56 +0100

    updated api-checker script

--- pre_commit_hooks/api_checker.py
+++ pre_commit_hooks/api_checker.py
@@ -7,29 +7,29 @@ import re
 
 def main(argv: Sequence[str] | None = None) -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', action='store', default=None, help='YAML-formatted input variable file')
-    parser.add_argument('-o', '--output', action='store', default="API.md", help='Markdown-formatted API documentation file')
+    parser.add_argument('filenames', nargs='*', help='Filenames to check.')
     args = parser.parse_args(argv)
 
     if args.input == None:
         return 1
 
     retval = 0
-    try:
-        with open(args.input) as varfile:
-            vars = yaml.safe_load(varfile.read())
+    for filename in args.filenames:
+        try:
+            with open(filename) as varfile:
+                vars = yaml.safe_load(varfile.read())
 
-            with open(args.output) as docfile:
-                doc = docfile.read()
-                for key in vars.keys():
-                    pattern = f'<!-- block {key} -->'
-                    match = re.search(pattern, doc)
-                    if not bool(match):
-                        print(f'Found un-documented variable {key}, please update {args.output} file.')
-                        retval += 1
-    except SyntaxError as e:
-        print(e)
-        retval = 1
+                with open('API.md') as docfile:
+                    doc = docfile.read()
+                    for key in vars.keys():
+                        pattern = f'<!-- block {key} -->'
+                        match = re.search(pattern, doc)
+                        if not bool(match):
+                            print(f'Found un-documented variable {key}, please update {args.output} file.')
+                            retval += 1
+        except SyntaxError as e:
+            print(e)
+            retval = 1
 
     return retval
 
